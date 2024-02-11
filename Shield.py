2c5530e248d3e9d09a473da2d315985dd95ba823
Hoang Thong Pham <thongphamhoang12@gmail.com>
2024-02-11 18:24:10 -0500

    initial version

--- Shield.py
+++ Shield.py
@@ -6,7 +6,15 @@ from Equipment import Equipment
 
 
 class Shield(Equipment):
-    def __init__(self, shield):
-        self.name = shield
+    shield = {"Shield"}
+
+    #def __init__(self):
+        #self.name = shield
+
+    def get_armor_type(self):
+        if self.name in Shield.shield:
+            return "Shield"
+
+
     def get_armor_class(self):
         return 2
